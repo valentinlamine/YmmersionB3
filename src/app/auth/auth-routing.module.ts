da278421e20a19be5d88fe93a45230af8ba3befe
bdimitri <dimitri.brancourt@ynov.com>
2024-10-06 16:52:41 +0200

    component And routing for forgot password

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import {ForgotPasswordComponent} from "./components/forgot-password/forgot-password.component";
 
 const routes: Routes = [
   {
     path: 'login', component: LoginComponent
   },
   {
-    path: 'ForgotPassword', component: LoginComponent
+    path: 'forgotPassword', component: ForgotPasswordComponent
   },
   {
     path: 'register', component: RegisterComponent
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,12 +5,14 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { FormsModule } from '@angular/forms';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ForgotPasswordComponent
   ],
     imports: [
         CommonModule,
